37cbc40f723c223049db8099de07268474799f76
faizali76 <141487350+faizali76@users.noreply.github.com>
2024-12-24 09:24:00 +0530

    Add files via upload

---
+++ Chapter (7)/03_list_using_while.py
@@ -0,0 +1,6 @@
+# Write a program to print the content of a list using while loops.
+l = [1,"faiz","false","rohan","this","ali"]
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i +=1
---
+++ Chapter (7)/08_pass_statement.py
@@ -0,0 +1,7 @@
+for i in range(7633):
+    pass
+
+i = 0
+while(i<20):
+    print(i)
+    i +=1
