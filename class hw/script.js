8c15b3cc0eb11c1aa4667489bbee2a73909841a7
faizali76 <141487350+faizali76@users.noreply.github.com>
2025-05-24 17:21:53 +0530

    Added HTML and CSS Project files
    This is a basic front-end project built using HTML and CSS .It includes a homepage layout with styling and responsive design. Create for practice and learning purposes.

---
+++ class hw/script.js
@@ -0,0 +1,20 @@
+// // Smooth Scroll
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute('href'));
+//     if (target) {
+//       target.scrollIntoView({
+//         behavior: 'smooth'
+//       });
+//     }
+//   });
+// });
+
+// // Form Submit Alert
+// const form = document.querySelector('form');
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   alert("Thank you, your form has been submitted!");
+//   form.reset();
+// });
